d5277453c8c14cec7113ba2a0c6465ecc3cf2b9f
Christopher CHEN <chenshaoming@appgame.com>
2016-05-03 15:39:31 +0800

    feat: compatible with composer autoloader

--- bin/commands/ClearCacheCommand.php
+++ bin/commands/ClearCacheCommand.php
@@ -4,12 +4,25 @@ namespace Commands;
 use Phwoolcon\Cache;
 use Phwoolcon\Cli\Command;
 use Phwoolcon\Config;
+use Symfony\Component\Console\Input\InputOption;
 
 class ClearCacheCommand extends Command
 {
 
+    protected function configure()
+    {
+        $this->setDefinition([
+            new InputOption('config-only', 'c', InputOption::VALUE_NONE, 'Clear config cache only'),
+        ])->setDescription('Clears cache');
+    }
+
     public function fire()
     {
+        if ($this->input->getOption('config-only')) {
+            Config::clearCache();
+            $this->info('Config cache cleared.');
+            return;
+        }
         Cache::flush();
         Config::clearCache();
         $this->info('Cache cleared.');
