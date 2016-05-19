2b67c3e071ca510057dfe16bf32c9d6d1be81549
Christopher CHEN <chenshaoming@appgame.com>
2016-05-19 21:28:40 +0800

    feat: add queue failed logger config; declare adapter classes explicitly

--- app/config/queue.php
+++ app/config/queue.php
@@ -2,15 +2,33 @@
 
 return [
     'default' => 'default_queue',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Queues
+    |--------------------------------------------------------------------------
+    |
+    | Configure the queues by purpose here.
+    |
+    */
     'queues' => [
         'default_queue' => [
             'connection' => 'beanstalkd',
             'options' => [],
         ],
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Queue Connections
+    |--------------------------------------------------------------------------
+    |
+    | Configure the connection information for each queue server.
+    |
+    */
     'connections' => [
         'beanstalkd' => [
-            'adapter' => 'Beanstalkd',
+            'adapter' => 'Phwoolcon\Queue\Adapter\Beanstalkd',
             'host' => '127.0.0.1',
             'port' => 11300,
             'connect_timeout' => 5,
@@ -20,6 +38,23 @@ return [
         'file' => [
             'path' => ROOT_PATH . '/storage/queue',
             'ext' => '.data',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Failed Queue Jobs
+    |--------------------------------------------------------------------------
+    |
+    | Configure the behavior of failed queue job logging so you
+    | can store the jobs that have failed.
+    |
+    */
+    'failed_logger' => [
+        'adapter' => 'Phwoolcon\Queue\FailedLoggerDb',
+        'options' => [
+            'connection' => '',
+            'table' => 'failed_jobs',
         ],
     ],
 ];
