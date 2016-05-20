2ce37fc58b789f7264b60a363199e2c1d778e103
Christopher CHEN <chenshaoming@appgame.com>
2016-05-20 20:27:06 +0800

    fix: nav toggle in narrow screen

--- public/assets/base/js/body-js.js
+++ public/assets/base/js/body-js.js
@@ -3,4 +3,12 @@
         $p.cookie('locale', this.value);
         w.location.reload();
     });
+    var nav = $('header nav');
+    nav.on('click', function (e) {
+        nav.addClass('active');
+        e.stopPropagation();
+    });
+    $(d).on('click', function (e) {
+        nav.removeClass('active');
+    });
 }(window, document, jQuery);
