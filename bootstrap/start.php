499170aed2623e3f1852932a0df30cb65fe21c92
Christopher CHEN <chenshaoming@appgame.com>
2016-09-12 22:36:49 +0800

    refactor: define environment variables first in start.php

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -3,17 +3,17 @@
 use Phalcon\Di\FactoryDefault;
 use Phalcon\Loader;
 
+defined('ROOT_PATH') or define('ROOT_PATH', dirname(__DIR__));
+$_SERVER['PHWOOLCON_ROOT_PATH'] = ROOT_PATH;
+$_SERVER['PHWOOLCON_CONFIG_PATH'] = ROOT_PATH . '/app/config';
+
 if (!extension_loaded('phalcon')) {
     echo $error = 'Extension "phalcon" not detected, please install or activate it.';
     throw new RuntimeException($error);
 }
 
-defined('ROOT_PATH') or define('ROOT_PATH', dirname(__DIR__));
-
 // The FactoryDefault Dependency Injector automatically register the right services providing a full stack framework
 $di = new FactoryDefault();
-$_SERVER['PHWOOLCON_ROOT_PATH'] = ROOT_PATH;
-$_SERVER['PHWOOLCON_CONFIG_PATH'] = ROOT_PATH . '/app/config';
 
 if (!is_file($includes = ROOT_PATH . '/vendor/composer/autoload_phalcon_files.php')) {
     echo $error = sprintf('Autoload not ready, please run command "%s/bin/dump-autoload" first.', ROOT_PATH);
