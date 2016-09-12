3bd11128143ab2020365d4a0cc5720d0a9682faa
Christopher CHEN <chenshaoming@appgame.com>
2016-09-12 15:44:20 +0800

    refactor: define commands in packages

--- app/config/commands.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'clear:cache' => 'Commands\ClearCacheCommand',
-    'migrate' => 'Phwoolcon\Cli\Command\Migrate',
-    'migrate:create' => 'Phwoolcon\Cli\Command\MigrateCreate',
-    'migrate:revert' => 'Phwoolcon\Cli\Command\MigrateRevert',
-    'migrate:list' => 'Phwoolcon\Cli\Command\MigrateList',
-    'service' => 'Phwoolcon\Cli\Command\ServiceCommand',
-];
--- bin/commands/ClearCacheCommand.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Commands;
-
-use Phwoolcon\Cli\Command\ClearCacheCommand as PhwoolconClearCacheCommand;
-
-class ClearCacheCommand extends PhwoolconClearCacheCommand
-{
-}
